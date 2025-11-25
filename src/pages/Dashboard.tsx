13167543e409a37d0334f8e8052210dfca867bf8
theloneson <l.o.n.e.s.o.n456@gmail.com>
2025-11-24 18:02:38 -0600

    updated dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -31,37 +31,30 @@ const Index = () => {
       isWalletConnected={isWalletConnected} 
       onConnectWallet={handleWalletConnect}
     >
-      {/* Single Screen Grid - Reduced height for some components */}
       <div className="w-full h-[calc(100vh-4rem)]">
         <div className="grid grid-cols-3 grid-rows-2 gap-4 h-full p-4">
-          
-          {/* Performance Chart - Top Left (2x1) - Full Height */}
           <div className="col-span-2 row-span-1">
             <AdvancedPerformanceChart isWalletConnected={isWalletConnected} />
           </div>
           
-          {/* Performance Metrics - Top Right (1x1) - Full Height */}
           <div className="col-span-1 row-span-1">
             <PerformanceMetrics isWalletConnected={isWalletConnected} />
           </div>
 
-          {/* Recent Activities - Bottom Left (1x1) - Reduced Height */}
           <div className="col-span-1 row-span-1">
-            <div className="h-64"> {/* Reduced from full height */}
+            <div className="h-64"> 
               <RecentActivities isWalletConnected={isWalletConnected} />
             </div>
           </div>
 
-          {/* AI Insights - Bottom Right (2x1) - Reduced Height */}
           <div className="col-span-2 row-span-1">
-            <div className="h-64"> {/* Reduced from full height */}
+            <div className="h-64"> 
               <AIInsightCards isWalletConnected={isWalletConnected} />
             </div>
           </div>
         </div>
       </div>
 
-      {/* Dialogs */}
       <ConnectWalletDialog 
         open={showWalletDialog} 
         onOpenChange={setShowWalletDialog}
